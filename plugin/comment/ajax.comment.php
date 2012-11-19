d49ca2fa460a98fc8c998cf76aa9d62cc9e0e5e0
Lei Peng <pyp126@gmail.com>
2012-11-19 10:54:42 +0800

    Add permission control to commit

--- plugin/comment/ajax.comment.php
+++ plugin/comment/ajax.comment.php
@@ -45,7 +45,7 @@ switch($_POST['action'])
 					echo "<div class='comment_item'>
 					<div class='padding'>#$floor
 					";
-					if(log_check())
+					if(user_level()<=1 && user_level()>=0)
 					{
 						echo "<a href='#' onclick='comment_del(\"".$arycmt[$i]['time']."\")'>(X)</a> ";
 					}
